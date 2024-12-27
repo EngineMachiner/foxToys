b56878416f2457ecffbccdb90f46a3d100d0dac0
EngineMachiner <mfpc98@gmail.com>
2024-12-26 22:54:34 -0300

    Split things into BGAnimations
    Split the gameplay's fail, clear and intro animations to ease up things.

---
+++ foxToys/PSX/DDR/ScreenGameplay cleared/default.lua
@@ -0,0 +1,42 @@
+
+local function quad()
+
+    return Def.Quad{
+        InitCommand=function(self) self:FullScreen():diffuse(Color.Black) end
+    }
+
+end
+
+local title = Def.ActorFrame{
+
+    InitCommand=function(self) self:visible(false) end,
+    OnCommand=function(self) self:sleep(2.25):queuecommand("Show") end,
+    ShowCommand=function(self) self:visible(true) end,
+
+    Def.Sprite{
+
+        Texture='1.png',
+        
+        InitCommand=function(self)
+            self:zoom(3):Center():SetTextureFiltering(false)
+        end,
+
+        ShowCommand=function(self)
+            self:sleep(3):linear(2):diffusealpha(0):sleep(1)
+        end
+
+    },
+
+    quad() .. {
+
+        ShowCommand=function(self)
+
+            self:y( SCREEN_HEIGHT * 0.125 )         self:fadebottom(0):linear(4):fadebottom(1)
+
+        end
+
+    }
+
+}
+
+return Def.ActorFrame{ LoadModule( "foxToys/PSX/DDR/Stars/default.lua", 2 ),     title }
--- foxToys/PSX/DDR/Stars/default.lua
+++ foxToys/PSX/DDR/Stars/default.lua
@@ -1,12 +1,81 @@
 
-local t = Def.ActorFrame{} -- First stars.
-local t2 = Def.ActorFrame{} -- Failed and cleared stars.
+local starType = ... or 1
 
-local zoom = SCREEN_HEIGHT / 240;   local n = 8
+local t = Def.ActorFrame{} -- Failed and cleared stars.
+local t2 = Def.ActorFrame{} -- Stars at the beginning.
+
+local zoom = SCREEN_HEIGHT / 240;       local n = 8
 
 for j = 1, 2 do for i = 1, n do
 
     t[#t+1] = Def.ActorFrame{
+        
+        Def.Quad{ InitCommand=function(self) self:GetParent().Quad = self end },
+
+        Def.Sprite{
+
+            Texture='2 1x3.png',
+            InitCommand=function(self)
+
+                local i = i - 1
+
+                self:zoom(zoom):setstate(1):animate(false)
+
+                local w, h = self:GetZoomedWidth(), self:GetZoomedHeight()
+
+                local x = - ( SCREEN_WIDTH * 2 - w ) * 0.5
+
+                if j == 2 then x = - x else self:rotationy(180) end
+
+                self:x(x):SetTextureFiltering(false):blend('add')
+
+                local p = self:GetParent();     local q = p.Quad
+
+                local a = math.abs( i - n * 0.5 + 0.5 )
+                x = w * ( a - n + 1 ) * 0.5
+                x = SCREEN_WIDTH * 2 - x
+
+                if j == 2 then
+                    a = math.abs( i - n * 0.5 + 1 )
+                    x = w * ( - a - n + 1 ) * 0.5
+                    x = - SCREEN_WIDTH * 2 - x
+                else p:y( - h ) end
+
+                p:xy( x, p:GetY() + h * ( i * 2 + 1.5 ) )
+
+                q:setsize( SCREEN_WIDTH * 2, h ):diffuse(Color.Black)
+
+                if starType == 1 then self:playcommand("PlayerFailed") end
+
+            end,
+
+            PlayerFailedCommand=function(self)
+
+                local p = self:GetParent():GetParent()
+
+                p:zoomx(-1):x(SCREEN_WIDTH)
+
+                self:setstate(0):queuecommand("Off")
+
+            end,
+
+            OffCommand=function(self)
+                
+                local p = self:GetParent()
+                local t = SCREEN_WIDTH / SCREEN_HEIGHT
+                local x = - SCREEN_WIDTH * 2
+
+                if j == 2 then x = - x end
+
+                p:linear( t * 1.5 ):x( p:GetX() + x )
+
+            end
+
+        }
+
+    }
+
+    t2[#t2+1] = Def.ActorFrame{
         
         Def.Quad{ InitCommand=function(self) self:GetParent().Quad = self end },
 
@@ -61,73 +130,12 @@ for j = 1, 2 do for i = 1, n do
 
     }
 
-    t2[#t2+1] = Def.ActorFrame{
-        
-        Def.Quad{ InitCommand=function(self) self:GetParent().Quad = self end },
-
-        Def.Sprite{
-
-            Texture='2 1x3.png',
-            InitCommand=function(self)
-
-                local i = i - 1
-
-                self:zoom(zoom):setstate(1):animate(false)
-
-                local w, h = self:GetZoomedWidth(), self:GetZoomedHeight()
-
-                local x = - ( SCREEN_WIDTH * 2 - w ) * 0.5
-
-                if j == 2 then x = - x else self:rotationy(180) end
-
-                self:x(x):SetTextureFiltering(false):blend('add')
-
-                local p = self:GetParent();     local q = p.Quad
-
-                local a = math.abs( i - n * 0.5 + 0.5 )
-                x = w * ( a - n + 1 ) * 0.5
-                x = SCREEN_WIDTH * 2 - x
-
-                if j == 2 then
-                    a = math.abs( i - n * 0.5 + 1 )
-                    x = w * ( - a - n + 1 ) * 0.5
-                    x = - SCREEN_WIDTH * 2 - x
-                else p:y( - h ) end
-
-                p:xy( x, p:GetY() + h * ( i * 2 + 1.5 ) )
-
-                q:setsize( SCREEN_WIDTH * 2, h ):diffuse(Color.Black)
-
-            end,
-
-            PlayerFailedMessageCommand=function(self)
-
-                local p = self:GetParent():GetParent()
-
-                p:zoomx(-1):x(SCREEN_WIDTH)
-
-                self:setstate(0):queuecommand("Off")
-
-            end,
-
-            OffCommand=function(self)
-                
-                local p = self:GetParent()
-                local t = SCREEN_WIDTH / SCREEN_HEIGHT
-                local x = - SCREEN_WIDTH * 2
-
-                if j == 2 then x = - x end
-
-                p:linear( t * 1.5 ):x( p:GetX() + x )
-
-            end
-
-        }
-
-    }
-
 end end
 
-t[#t+1] = Def.Quad { InitCommand=function(self) self:diffuse( Color.Black ):FullScreen():MaskDest() end }
+t2[#t2+1] = Def.Quad { 
+    
+    InitCommand=function(self) self:diffuse( Color.Black ):FullScreen():MaskDest() end 
 
-return Def.ActorFrame{ t, t2 }
+}
+
+if starType == 3 then return Def.ActorFrame{ t2 } end             return Def.ActorFrame{ t }
